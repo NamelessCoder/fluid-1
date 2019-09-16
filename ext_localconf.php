4f17a928ccddb17a5ad734e3f1d36602c74e4dcc
Claus Due <claus@namelesscoder.net>
2019-09-16 14:43:11 +0200

    [TASK] Convert expression node and pre-processor configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,13 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-unset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['fluid_template']);
+unset(
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['fluid_template'],
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['preProcessors']
+);
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['expressionNodeTypes'] = [
+    \TYPO3Fluid\Fluid\ViewHelpers\Expression\CastViewHelper::class,
+    \TYPO3Fluid\Fluid\ViewHelpers\Expression\MathViewHelper::class,
+    \TYPO3Fluid\Fluid\ViewHelpers\IfViewHelper::class
+];
